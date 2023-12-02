e5567f196b198d4907530bb5bf43fb6be8565fd4
shebll <shebblloll@gmail.com>
2023-12-02 20:19:45 +0200

    fix api fetching

--- app/(site)/product/page.tsx
+++ app/(site)/product/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Products from "../components/ProductsMachinesPage";
 import ProductMachineType from "@/types/Product";
-type props = {
-  products: ProductMachineType[];
-};
-async function page(props: props) {
+// type props = {
+//   products: ProductMachineType[];
+// };
+async function page() {
   const response = await fetch("http://localhost:3000/api/products", {
     cache: "no-cache",
   });
@@ -12,20 +12,20 @@ async function page(props: props) {
   console.log(products.products);
   return (
     <div>
-      <Products items={props.products} />
+      <Products items={products.products} />
     </div>
   );
 }
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3000/api/products", {
-    cache: "no-cache",
-  });
-  const data = await response.json();
+// export async function getStaticProps() {
+//   const response = await fetch("http://localhost:3000/api/products", {
+//     cache: "no-cache",
+//   });
+//   const data = await response.json();
 
-  return {
-    props: {
-      products: data.products,
-    },
-  };
-}
+//   return {
+//     props: {
+//       products: data.products,
+//     },
+//   };
+// }
 export default page;
